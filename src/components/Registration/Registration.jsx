eafb31149e2abda1a25a592e204309ec855b40bb
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2024-10-29 15:21:11 +0100

    Update Registration.jsx
    simplify registration

--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -1,10 +1,9 @@
-import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { registerAPI } from "../../redux/auth/api";
 import { logIn } from "../../redux/auth/operations";
 import { OrangeButton } from "../ModalButtons/OrangeButton";
 import {
-  Form,
+  FormWrapper,
   Text,
   Label,
   Input,
@@ -14,24 +13,18 @@ import {
 } from "./Registration.styled";
 
 export const RegistrationForm = () => {
-  // State
-  const [emailDirty, setEmailDirty] = useState(false);
-  const [passwordDirty, setPasswordDirty] = useState(false);
-  const [emailError, setEmaiError] = useState("This is a required field");
-  const [passwordError, setPasswordError] = useState(
-    "This is a required field"
-  );
-  const [errorSymbol, setErrorSymbol] = useState("*");
-  // Dispatch
   const dispatch = useDispatch();
-  // Handle Submit
-  const handleSubmit = async (event) => {
+
+  const handleSubmit = (event) => {
     event.preventDefault();
-    const { email, password } = event.target.elements;
-    const data = await registerAPI({
+
+    const { email, password } = event.currentTarget.elements;
+
+    const data = registerAPI({
       email: email.value,
       password: password.value,
     });
+
     if (data) {
       dispatch(
         logIn({
@@ -42,100 +35,33 @@ export const RegistrationForm = () => {
     }
   };
 
-  const blurHandler = (e) => {
-    switch (e.target.name) {
-      case "email":
-        setEmailDirty(true);
-        break;
-      case "password":
-        setPasswordDirty(true);
-        break;
-      default:
-        return;
-    }
-  };
-  const emailHandler = (e) => {
-    const re =
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (!re.test(String(e.target.value).toLowerCase())) {
-      setEmaiError("Incorrect email");
-      setErrorSymbol("*");
-      if (!e.target.value) {
-        setEmaiError("This is a required field");
-        setErrorSymbol("*");
-      }
-    } else {
-      setEmaiError("");
-    }
-  };
-
-  const passwordHandler = (e) => {
-    if (e.target.value.length < 8) {
-      setPasswordError("Password must be at least 8 characters");
-      if (!e.target.value) {
-        setPasswordError("This is a required field");
-      }
-    } else {
-      setPasswordError("");
-    }
-  };
-
   return (
-    <Form onSubmit={handleSubmit}>
+    <FormWrapper>
       <Text>Fill in the fields to register</Text>
 
-      <form action="" autoComplete="on">
+      <form onSubmit={handleSubmit} autoComplete="on">
         <Label>
-          <LabelText>
-            {emailDirty && emailError && (
-              <span style={{ color: "#EB5757", fontSize: 10, paddingTop: 4 }}>
-                {errorSymbol}{" "}
-              </span>
-            )}
-            Email:
-          </LabelText>
+          <LabelText>Email:</LabelText>
           <Input
-            onBlur={blurHandler}
-            onChange={emailHandler}
             type="email"
             name="email"
+            autoComplete="email"
             placeholder="your@email.com"
-            pattern="[A-Za-zА-Яа-яЁёЄєЇї0-9._%+-]+@[A-Za-zА-Яа-яЁёЄєЇї0-9.-]+\.[A-Za-zА-Яа-яЁёЄєЇї]{2,4}$"
             title="Email may consist of letters, numbers and a mandatory character '@'"
             required
           />
-          {emailDirty && emailError && (
-            <div style={{ color: "#EB5757", fontSize: 10, paddingTop: 4 }}>
-              {emailError}{" "}
-            </div>
-          )}
         </Label>
         <div>
           <Label>
-            <LabelText>
-              {passwordDirty && passwordError && (
-                <span style={{ color: "#EB5757", fontSize: 10, paddingTop: 4 }}>
-                  {errorSymbol}{" "}
-                </span>
-              )}
-              Password:
-            </LabelText>
+            <LabelText>Password:</LabelText>
             <Input
-              onBlur={blurHandler}
-              onChange={passwordHandler}
               type="password"
               name="password"
-              placeholder="password"
-              pattern="[0-9A-Za-zА-Яа-яЁёЄєЇї!@#$%^&*]{8,}"
+              autoComplete="current-password"
+              placeholder="Select your password..."
               title="The password can consist of at least 8 letters, numbers and symbols '!@#$%^&*'"
               required
             />
-
-            {passwordDirty && passwordError && (
-              <div style={{ color: "#EB5757", fontSize: 10, paddingTop: 4 }}>
-                {passwordError}{" "}
-              </div>
-            )}
           </Label>
         </div>
         <ButtonBox>
@@ -143,6 +69,6 @@ export const RegistrationForm = () => {
           <OrangeButton type="submit">REGISTRATION</OrangeButton>
         </ButtonBox>
       </form>
-    </Form>
+    </FormWrapper>
   );
 };
