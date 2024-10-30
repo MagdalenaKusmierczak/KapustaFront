c6277475e3fbe9fd8c9787d1d696f705c8a5e3a7
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2024-10-30 13:01:06 +0100

    Update SharedLayout-styled.jsx

--- src/pages/SharedLayout/SharedLayout-styled.jsx
+++ src/pages/SharedLayout/SharedLayout-styled.jsx
@@ -1,14 +1,13 @@
 import styled from "styled-components";
 
 export const SharedLayoutContainer = styled.div`
-  display: flex;
-  box-shadow: 5px 10px 20px 0px #aab2c566;
-  flex-direction: column;
-  height: 100vh;
   width: 320px;
-  margin: 0px auto;
+  height: 100vh;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  box-shadow: 5px 10px 20px 0px #aab2c566;
   background-color: rgba(255, 255, 255, 1);
-
   @media screen and (min-width: 768px) {
     width: 768px;
   }
