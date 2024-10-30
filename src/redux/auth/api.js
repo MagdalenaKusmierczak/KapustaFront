cbbb56b7da5deebe06c77558ce49f71975271a8a
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2024-10-30 14:05:50 +0100

    Update api.js
    fixing routes

--- src/redux/auth/api.js
+++ src/redux/auth/api.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 import { Report } from "notiflix/build/notiflix-report-aio";
-axios.defaults.baseURL = "https://demokraci-kapusta.onrender.com/";
+axios.defaults.baseURL =
+  "https://kapusta-backend-fq38.onrender.com";
 
 export const registerAPI = async (user) => {
   try {
-    const { data } = await axios.post("/register", user);
+    const { data } = await axios.post("/auth/register", user);
     return data;
   } catch (error) {
     if (error.response.status === 409) {
@@ -14,24 +15,24 @@ export const registerAPI = async (user) => {
 };
 
 export const loginAPI = async (user) => {
-  const { data } = await axios.post("/login", user);
+  const { data } = await axios.post("/auth/login", user);
   return data;
 };
 
 export const logoutAPI = async () => {
-  const { data } = await axios.post("auth/logout");
+  const { data } = await axios.post("/auth/logout");
   return data;
 };
 
-export const googleLoginAPI = async () => {
-  const response = await axios.get("/auth/google", {
-    headers: {
-      accept: "*/*",
-    },
-  });
-  console.log("response", response);
-  return response;
-};
+// export const googleLoginAPI = async () => {
+//   const response = await axios.get("/auth/google", {
+//     headers: {
+//       accept: "*/*",
+//     },
+//   });
+//   console.log("response", response);
+//   return response;
+// };
 
 export const fullUserInfoAPI = async () => {
   const { data } = await axios.get("user");
