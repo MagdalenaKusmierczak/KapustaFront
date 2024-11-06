9b99f7d3bc1983022b97467442f63704570859f1
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2024-11-06 18:01:17 +0100

    Reports List
    fixed ReportsList

--- src/components/Reports/ReportsList/ReportsList.jsx
+++ src/components/Reports/ReportsList/ReportsList.jsx
@@ -1,118 +1,60 @@
-import { useState, useEffect, useMemo } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { selectReports } from "../../../redux/reports/selectors";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
 import { filteredDataAction } from "../../../redux/reportsQuery/reportsQuery.slice";
 import icons from "../../../assets/icons.svg";
-import { List, Item, ItemIncome, ItemSvg, BgcSvg } from "./ReportsList.styled";
+import { List, Item, ItemSvg, BgcSvg } from "./ReportsList.styled";
 
-export const ReportsList = ({ onChange }) => {
+export const ReportsList = ({ onChange, data }) => {
   const [active, setActive] = useState("");
-  const [data, setData] = useState({});
-
-  const { reports } = useSelector(selectReports);
 
   const dispatch = useDispatch();
 
   const valueArr = [];
 
-  const expensesData = useMemo(
-    () => reports?.expenses?.expensesData ?? {},
-    [reports]
-  );
-  
-  const incomesData = useMemo(
-    () => reports?.incomes?.incomesData ?? {},
-    [reports]
-  );
-  
-  useEffect(() => {
-    if (onChange === "expenses") {
-      setData(expensesData ?? {});
-      setActive("");
-    } else {
-      setData(incomesData ?? {});
-      setActive("");
-    }
-  }, [onChange, expensesData, incomesData]);
+  const entries = data && typeof data === "object" ? Object.entries(data) : [];
 
   const clickEventHandler = (event) => {
     setActive(event.currentTarget.id);
 
     const filteredValueArr = valueArr.filter(
-      (item) => item[0].replace(/\s+/g, "") === event.currentTarget.id
+      (item) => item[0] === event.currentTarget.id
     );
-
     dispatch(filteredDataAction(filteredValueArr));
   };
 
-  const entries = Object.entries(data) ?? [];
-
   return (
     <div>
-      <List className={onChange === "income" ? "incomeList" : ""}>
-        {entries.map((item) => {
-          const iconName = item[0].replace(/\s+/g, "");
-          valueArr.push(item);
-          if (onChange === "expenses") {
-            return (
-              <Item
-                key={iconName}
-                id={iconName}
-                onClick={clickEventHandler}
-                className={iconName === active ? "active" : ""}
+      <List>
+        {entries.map(([categoryName, entryData]) => {
+          const iconId = categoryName.replace(/[\s,]+/g, "");
+
+          valueArr.push([iconId, entryData]);
+
+          const isActive = iconId === active;
+
+          return (
+            <Item
+              key={iconId}
+              id={iconId}
+              onClick={clickEventHandler}
+              className={isActive ? "active" : ""}
+            >
+              <p>{entryData}.00</p>
+              <ItemSvg
+                width="60"
+                height="60"
+                style={{ color: isActive ? "#ff751d" : "currentColor" }}
               >
-                <p>{item[1].total}.00</p>
-                <ItemSvg width="56" height="56">
-                  <BgcSvg
-                    src={
-                      iconName === active
-                        ? `${icons}#orange-bg`
-                        : `${icons}#grey-bg`
-                    }
-                    width="59"
-                    height="46"
-                    viewBox="0 0 54 47"
-                    background-position="center"
-                    className={iconName === active ? "active" : ""}
-                  />
-                  <use href={`${icons}#${iconName}`}></use>
-                </ItemSvg>
-                <p>{item[0]}</p>
-              </Item>
-            );
-          } else if (onChange === "income") {
-            return (
-              <ItemIncome
-                key={iconName}
-                id={iconName}
-                onClick={clickEventHandler}
-                className={iconName === active ? "active" : ""}
-              >
-                <p>{item[1].total}.00</p>
-                <ItemSvg
-                  width="56"
-                  height="56"
-                  className={iconName === active ? "active" : ""}
-                >
-                  <BgcSvg
-                    src={
-                      iconName === active
-                        ? `${icons}#orange-bg`
-                        : `${icons}#grey-bg`
-                    }
-                    width="59"
-                    height="46"
-                    viewBox="0 0 54 47"
-                    background-position="center"
-                    className={iconName === active ? "active" : ""}
-                  />
-                  <use href={`${icons}#${iconName}`}></use>
-                </ItemSvg>
-                <p>{item[0]}</p>
-              </ItemIncome>
-            );
-          }
-          return <></>;
+                <BgcSvg width="60" height="60" viewBox="0 0 60 60">
+                  <use
+                    href={`${icons}#${isActive ? "orange-bg" : "grey-bg"}`}
+                  ></use>
+                </BgcSvg>
+                <use href={`${icons}#${iconId}`}></use>
+              </ItemSvg>
+              <p>{categoryName}</p>
+            </Item>
+          );
         })}
       </List>
     </div>
--- src/components/Reports/ReportsList/ReportsList.styled.jsx
+++ src/components/Reports/ReportsList/ReportsList.styled.jsx
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import { HandySvg } from "handy-svg";
 
 export const List = styled.ul`
   display: flex;
@@ -43,41 +42,12 @@ export const Item = styled.li`
   }
 `;
 
-export const ItemIncome = styled.li`
-  flex-grow: 1;
-  display: flex;
-  text-align: center;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  width: calc((100% - 20px) / 3);
-  margin: 0%;
-  padding: 20px 0;
-  gap: 5px;
-  border-bottom: 1px solid #e0e5eb;
-  &.active {
-    fill: #ff751d;
-  }
-  &:hover,
-  &:focus {
-    cursor: pointer;
-    fill: #ff751d;
-  }
-  @media screen and (min-width: 768px) {
-    border: none;
-    width: calc((100% - 64px) / 6);
-  }
-  max-width: 90px;
-  flex-grow: 0;
-`;
-
 export const ItemSvg = styled.svg`
   position: relative;
+  display: flex;
 `;
 
-export const BgcSvg = styled(HandySvg)`
+export const BgcSvg = styled.svg`
   position: absolute;
   top: 100px;
-  width: 59px;
-  height: 46px;
 `;
