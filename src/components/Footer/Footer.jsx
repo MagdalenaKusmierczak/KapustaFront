1de2900e0f060c07ff2a0f8b2e066878cf9b1955
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2024-11-07 16:24:46 +0100

    create footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,80 @@
+import { useLocation } from "react-router";
+import { useMatchMedia } from "../../hooks/MediaQuery";
+import githubIcon from "../../assets/github_icon.svg";
+import linkedInIcon from "../../assets/linkedin_icon.svg";
+import emailIcon from "../../assets/email_icon.svg";
+import {
+  FooterContainer,
+  AddressContainer,
+  AddressList,
+  Icon,
+  Paragraph,
+} from "./Footer.styled";
+
+const Footer = () => {
+  const location = useLocation();
+
+  const { isMobile } = useMatchMedia();
+
+  const isExpenses = location.pathname === "/expenses";
+
+  const isIncome = location.pathname === "/income";
+
+  const validator = (isExpenses && isMobile) || (isIncome && isMobile);
+
+     if (validator) {
+       return null; // Don't render the FooterContainer
+    }
+    
+  return (
+    <FooterContainer>
+      <Paragraph>
+        Developed by:<span>Magdalena Kuśmierczak</span>
+      </Paragraph>
+      <AddressContainer>
+        <AddressList>
+          <li>
+            <a
+              href="https://github.com/MagdalenaKusmierczak"
+              rel="noreferrer"
+              target="_blank"
+            >
+              <Icon
+                src={githubIcon}
+                alt="Github link"
+                width="25px"
+                height="25px"
+              />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://linkedin.com/in/magdalena-kusmierczak-4b94a7303"
+              rel="noreferrer"
+              target="_blank"
+            >
+              <Icon
+                src={linkedInIcon}
+                alt="LinkedIn link"
+                width="25px"
+                height="25px"
+              />
+            </a>
+          </li>
+          <li>
+            <a href="mailto:magdalenaxjare@gmail.com">
+              <Icon
+                src={emailIcon}
+                alt="Email address"
+                width="25px"
+                height="25px"
+              />
+            </a>
+          </li>
+        </AddressList>
+      </AddressContainer>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
