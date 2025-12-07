a38f0a7af09e3646df922e4958afb94f46575b70
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2025-12-07 18:56:07 +0100

    Extracted unifided auth component

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,7 @@ import SharedLayout from "./pages/SharedLayout/SharedLayout";
 const Reports = lazy(() => import("./pages/Reports/Reports"));
 const Expenses = lazy(() => import("./pages/Transactions/Expenses/Expenses"));
 const Income = lazy(() => import("./pages/Transactions/Incomes/Incomes"));
-const Login = lazy(() => import("./pages/AuthPages/Login/Login"));
-const RegisterPage = lazy(() => import("./pages//AuthPages/Register/Register"));
+const Auth = lazy(() => import("./pages/AuthPage/AuthPage"));
 
 const App = () => {
   const dispatch = useDispatch<AppDispatch>();
@@ -35,11 +34,11 @@ const App = () => {
         <Route index element={<Navigate to="/expenses" />} />
         <Route
           path="/login"
-          element={<RestrictedRoute component={<Login />} />}
+          element={<RestrictedRoute component={<Auth />} />}
         />
         <Route
           path="/register"
-          element={<RestrictedRoute component={<RegisterPage />} />}
+          element={<RestrictedRoute component={<Auth />} />}
         />
         <Route
           path="/expenses"
